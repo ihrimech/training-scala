f380cf1021db3c0bb226ce438cc24e4fd58ec74b
ismail <hrimechismail@gmail.com>
2021-06-19 14:06:09 +0200

    Correction 4

--- src/main/scala/Purchase.scala
+++ src/main/scala/Purchase.scala
@@ -1,4 +1,5 @@
 class Purchase(val merchandise: Merchandise, val hours: Int, val minutes: Int) {
   // TODO verifier que hours est comprise entre 0 et 23
   // TODO verifier que minutes est comprise entre 0 et 59
+  val asMinutes: Int = hours * 60 + minutes
 }
