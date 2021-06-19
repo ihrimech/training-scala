a97eb1ddc887d799a3ef95f92868b6ee04eb5632
ismail <hrimechismail@gmail.com>
2021-06-19 15:19:23 +0200

    Exercice 6

--- src/main/scala/Purchase.scala
+++ src/main/scala/Purchase.scala
@@ -2,4 +2,8 @@ class Purchase(val merchandise: Merchandise, val hours: Int, val minutes: Int) {
   // TODO verifier que hours est comprise entre 0 et 23
   // TODO verifier que minutes est comprise entre 0 et 59
   val asMinutes: Int = hours * 60 + minutes
+
+  def %(percentage: Double): Double = promotion(percentage)
+
+  def promotion(percentage: Double): Double = merchandise.price * percentage
 }
