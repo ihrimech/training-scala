0aeab79184d87c1a548ff9a55ccd2c909bdbe0ba
ismail <hrimechismail@gmail.com>
2021-06-19 13:59:15 +0200

    correction 3

---
+++ src/main/scala/Purchase.scala
@@ -0,0 +1,4 @@
+class Purchase(val merchandise: Merchandise, val hours: Int, val minutes: Int) {
+  // TODO verifier que hours est comprise entre 0 et 23
+  // TODO verifier que minutes est comprise entre 0 et 59
+}
