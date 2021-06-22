638bd86d18660a1124947679d49cf941471450ff
ismail <hrimechismail@gmail.com>
2021-06-23 01:31:25 +0200

    Solution 15

--- src/main/scala/com/myCompany/store/WebsiteStore.scala
+++ src/main/scala/com/myCompany/store/WebsiteStore.scala
@@ -4,5 +4,7 @@ import com.myCompany.merchandise.Merchandise
 
 case class WebsiteStore(merchandises: Map[Merchandise, Int]) {
   def addMerchandise(merchandise: Merchandise, quantity: Int) : WebsiteStore = WebsiteStore(merchandises + (merchandise -> quantity))
+
+  def resetQuantities: WebsiteStore = WebsiteStore(merchandises.map(merchandise => merchandise._1 -> 0))
 }
 
