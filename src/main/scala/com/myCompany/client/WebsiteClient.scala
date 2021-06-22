077fb29fba4f8eb95a29ef1f441ffb8a16a5a226
ismail <hrimechismail@gmail.com>
2021-06-23 01:19:59 +0200

    Correction 14

--- src/main/scala/com/myCompany/client/WebsiteClient.scala
+++ src/main/scala/com/myCompany/client/WebsiteClient.scala
@@ -1,3 +1,7 @@
 package com.myCompany.client
 
-case class WebsiteClient(name: String, verified: Boolean)
+case class WebsiteClient(name: String, verified: Boolean, purchases: Set[Purchase]) {
+  def addPurchase(purchase: Purchase): WebsiteClient = WebsiteClient(name, verified, purchases + purchase)
+
+  def removePurchase(purchase: Purchase): WebsiteClient = WebsiteClient(name, verified, purchases - purchase)
+}
--- src/main/scala/com/myCompany/store/WebsiteStore.scala
+++ src/main/scala/com/myCompany/store/WebsiteStore.scala
@@ -2,5 +2,7 @@ package com.myCompany.store
 
 import com.myCompany.merchandise.Merchandise
 
-case class WebsiteStore(merchandise: Merchandise, quantity: Int)
+case class WebsiteStore(merchandises: Map[Merchandise, Int]) {
+  def addMerchandise(merchandise: Merchandise, quantity: Int) : WebsiteStore = WebsiteStore(merchandises + (merchandise -> quantity))
+}
 
