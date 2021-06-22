9cc99dfbd9e0798e2524e3155a206fdf3ee85b1d
ismail <hrimechismail@gmail.com>
2021-06-22 21:55:01 +0200

    Correction 7

--- src/main/scala/Merchandise.scala
+++ src/main/scala/Merchandise.scala
@@ -2,4 +2,8 @@ class Merchandise (val name: String, val price: Double) {
   val description: String = name + " " + price
 
   def isCheap(cheapLimit: Double): Boolean = price < cheapLimit
+
+  def isCheaperThan(that: Merchandise): Boolean = this.price < that.price
+
+  def <(that: Merchandise): Boolean = isCheaperThan(that)
 }
--- src/main/scala/Purchase.scala
+++ src/main/scala/Purchase.scala
@@ -1,4 +1,4 @@
-class Purchase(val merchandise: Merchandise, val hours: Int, val minutes: Int) {
+class Purchase(val merchandise: Merchandise, val hours: Int = 0, val minutes: Int = 0) {
   // TODO verifier que hours est comprise entre 0 et 23
   // TODO verifier que minutes est comprise entre 0 et 59
   val asMinutes: Int = hours * 60 + minutes
