983844fc75d53ef8caa1272cc0f0f513ffdccb7c
ismail <hrimechismail@gmail.com>
2021-06-22 23:49:35 +0200

    Correction 10

--- src/main/scala/com/myCompany/purchase/Purchase.scala
+++ src/main/scala/com/myCompany/purchase/Purchase.scala
@@ -4,8 +4,9 @@ import com.myCompany.merchandise.Merchandise
 import com.myCompany.store.WebsiteStore
 
 class Purchase(val merchandise: Merchandise, val hours: Int = 0, val minutes: Int = 0) {
-  // TODO verifier que hours est comprise entre 0 et 23
-  // TODO verifier que minutes est comprise entre 0 et 59
+  require(hours >= 0 && hours < 24, "hours must be between 0 and 23")
+  require(minutes >= 0 && minutes < 59, "minutes must be between 0 and 59")
+
   val asMinutes: Int = hours * 60 + minutes
 
   def %(percentage: Double): Double = promotion(percentage)
