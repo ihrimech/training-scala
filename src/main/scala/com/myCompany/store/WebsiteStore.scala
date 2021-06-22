285175467a2bbbcfb840970612dc9e41c495517d
ismail <hrimechismail@gmail.com>
2021-06-23 01:48:29 +0200

    Correction 16

--- src/main/scala/com/myCompany/store/WebsiteStore.scala
+++ src/main/scala/com/myCompany/store/WebsiteStore.scala
@@ -1,10 +1,13 @@
 package com.myCompany.store
 
+import com.myCompany.client.WebsiteClient
 import com.myCompany.merchandise.Merchandise
 
-case class WebsiteStore(merchandises: Map[Merchandise, Int]) {
-  def addMerchandise(merchandise: Merchandise, quantity: Int) : WebsiteStore = WebsiteStore(merchandises + (merchandise -> quantity))
+case class WebsiteStore(merchandises: Map[Merchandise, Int], clients: Set[WebsiteClient]) {
+  def addMerchandise(merchandise: Merchandise, quantity: Int) : WebsiteStore = WebsiteStore(merchandises + (merchandise -> quantity), clients)
 
-  def resetQuantities: WebsiteStore = WebsiteStore(merchandises.map(merchandise => merchandise._1 -> 0))
+  def resetQuantities: WebsiteStore = WebsiteStore(merchandises.map(merchandise => merchandise._1 -> 0), clients)
+
+  def soldProducts: Set[String] = clients.flatMap(_.purchases.map(_.merchandise.name))
 }
 
