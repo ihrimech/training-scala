997182ca39849b58b8147038aa5c6557452ae46e
ismail <hrimechismail@gmail.com>
2021-06-25 01:37:58 +0200

    Correction 23

--- src/main/scala/com/myCompany/client/Purchase.scala
+++ src/main/scala/com/myCompany/client/Purchase.scala
@@ -1,6 +1,7 @@
 package com.myCompany.client
 
 import com.myCompany.merchandise.Merchandise
+import com.myCompany.payment.{BankTransfer, Check, CreditCard, PaymentMethods}
 import com.myCompany.store.WebsiteStore
 
 class Purchase(val merchandise: Merchandise, val hours: Int = 0, val minutes: Int = 0) {
@@ -12,4 +13,33 @@ class Purchase(val merchandise: Merchandise, val hours: Int = 0, val minutes: In
   def %(percentage: Double): Double = promotion(percentage)
 
   def promotion(percentage: Double): Double = merchandise.price * percentage
+
+  def makePayment(paymentMethod: PaymentMethods): String = (paymentByCheck orElse paymentByCard orElse paymentByBankTransfer)(paymentMethod)
+
+  val paymentByCard = new PartialFunction[PaymentMethods, String] {
+    override def isDefinedAt(x: PaymentMethods): Boolean = x match {
+      case _:CreditCard => true
+      case _ => false
+    }
+
+    override def apply(v1: PaymentMethods): String = "Payment accepted"
+  }
+
+  val paymentByCheck = new PartialFunction[PaymentMethods, String] {
+    override def isDefinedAt(x: PaymentMethods): Boolean = x match {
+      case _:Check => true
+      case _ => false
+    }
+
+    override def apply(v1: PaymentMethods): String = "Payment refused"
+  }
+
+  val paymentByBankTransfer = new PartialFunction[PaymentMethods, String] {
+    override def isDefinedAt(x: PaymentMethods): Boolean = x match {
+      case _:BankTransfer => true
+      case _ => false
+    }
+
+    override def apply(v1: PaymentMethods): String = "Payment pending"
+  }
 }
